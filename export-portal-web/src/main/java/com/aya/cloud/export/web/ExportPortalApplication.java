a09b52781d2a33e1a64135442cbf3084a209ef44
sheldn <sherrysheldn@gmail.com>
2023-06-12 23:13:24 +0800

    初始化工程

---
+++ export-portal-web/src/main/java/com/aya/cloud/export/web/ExportPortalApplication.java
@@ -0,0 +1,13 @@
+package com.aya.cloud.export.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExportPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExportPortalApplication.class, args);
+	}
+
+}
